94c154df85304533ec7421f67598a61557e3f42a
Torrekie <adeade2015@outlook.com>
2021-07-08 16:05:59 +0800

    Add support for opening auth page on jailbroken ios

--- cmdutil/lrUtils.go
+++ cmdutil/lrUtils.go
@@ -44,6 +44,8 @@ func Openbrowser(url string) {
 		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
 	case "darwin":
 		err = exec.Command("open", url).Start()
+	case "ios":
+		err = exec.Command("uiopen", url).Start()
 	default:
 		err = fmt.Errorf("unsupported platform")
 	}
